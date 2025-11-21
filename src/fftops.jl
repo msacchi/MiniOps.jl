d452bb503e712a2161e4159f48070267eb29b050
Mauricio Sacchi <mauriciosacchi@Mauricios-MacBook-Pro.local>
2025-11-20 19:02:40 -0700

    add pad_ip ND

--- src/fftops.jl
+++ src/fftops.jl
@@ -34,3 +34,82 @@ function fft_op(x0; unitary = true)
 
     return Op(f, ft; m = M, n = M, name = :fft_planned)
 end
+
+
+
+function pad_nd(x::AbstractArray,
+                pad_before::NTuple{N,Int},
+                pad_after::NTuple{N,Int}) where {N}
+
+    @assert ndims(x) == N
+    @assert length(pad_before) == N == length(pad_after)
+
+    out_sizes = ntuple(i -> pad_before[i] + size(x, i) + pad_after[i], N)
+    y = zeros(eltype(x), out_sizes)
+
+    ranges = ntuple(i -> (pad_before[i] + 1):(pad_before[i] + size(x, i)), N)
+    @inbounds y[ranges...] .= x
+
+    return y
+end
+
+function unpad_nd(y::AbstractArray,
+                  pad_before::NTuple{N,Int},
+                  pad_after::NTuple{N,Int}) where {N}
+
+    @assert ndims(y) == N
+    @assert length(pad_before) == N == length(pad_after)
+
+    in_sizes = ntuple(i -> size(y, i) - pad_before[i] - pad_after[i], N)
+    @assert all(s -> s > 0, in_sizes)
+
+    ranges = ntuple(i -> (pad_before[i] + 1):(pad_before[i] + in_sizes[i]), N)
+
+    x = similar(y, eltype(y), in_sizes)
+    @inbounds x .= @view y[ranges...]
+
+    return x
+end
+
+"""
+    pad_op(sz_in, pad_before, pad_after)
+
+Create an N-D padding operator for arrays of size `sz_in`.
+
+- `sz_in`      :: NTuple{N,Int}
+- `pad_before` :: NTuple{N,Int}
+- `pad_after`  :: NTuple{N,Int}
+"""
+function pad_op(sz_in::NTuple{N,Int},
+                pad_before::NTuple{N,Int},
+                pad_after::NTuple{N,Int}) where {N}
+
+    @assert length(pad_before) == N == length(pad_after)
+
+    # Output size of padded array
+    sz_out = ntuple(i -> pad_before[i] + sz_in[i] + pad_after[i], N)
+
+    # Flattened sizes for Op bookkeeping
+    n = prod(sz_in)   # input dof
+    m = prod(sz_out)  # output dof
+
+    f = function (x)
+        @assert size(x) == sz_in
+        pad_nd(x, pad_before, pad_after)
+    end
+
+    ft = function (y)
+        @assert size(y) == sz_out
+        unpad_nd(y, pad_before, pad_after)
+    end
+
+    return Op(f, ft; m = m, n = n, name = :pad_op)
+end
+
+# Convenience: pass a prototype array instead of sz_in
+pad_op(x0::AbstractArray,
+       pad_before::NTuple{N,Int},
+       pad_after::NTuple{N,Int}) where {N} =
+    pad_op(size(x0), pad_before, pad_after)
+
+
